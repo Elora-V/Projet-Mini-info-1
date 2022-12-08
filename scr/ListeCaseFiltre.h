85604e2c2af566da2547981648ba3ba5a682e18b
Elora-V <116489631+Elora-V@users.noreply.github.com>
2022-12-08 16:15:39 +0100

    Add files via upload

---
+++ scr/ListeCaseFiltre.h
@@ -0,0 +1,9 @@
+
+
+//A faire
+
+int listaNCase(coord position , int n , caseMvt tab[]);
+
+int filtreCaseSansPuc( caseMvt tab[],  nbCaseRempli , potager potager);
+
+int filtreCaseAvecTomate( caseMvt tab[],  nbCaseRempli , potager potager);
