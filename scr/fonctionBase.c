badf5bb753e321a50a1789baaf2409acf8640aac
Elora-V <elora95.vigo@gmail.com>
2022-12-08 16:11:29 +0100

    organisation fichier et ecriture liste case et filtre

---
+++ scr/fonctionBase.c
@@ -0,0 +1,43 @@
+#include "structure.h"
+#include "fonctionBase.h"
+	
+void EchangeTabCaseMvt( caseMvt tab[] , indice1 , indice2 ){
+	
+	//on garde en mémoire les valeurs à l'indice 1
+	int x1= tab[indice1].position.x;
+	int y1= tab[indice1].position.y;
+	int mvt1= tab[indice1].mvt;
+	
+	//on met les informations de indice 2 à indice 1
+	tab[indice1]=tab[indice2];
+	
+	//on ecrit les informations de indice 1 dans indice 2
+	tab[indice2]={ {x1,y1} , mvt1 };
+	
+}	
+
+
+int VerifSiTomate(coord position, potager potager){
+	int flag=1;
+	int x= position.x;
+	int y= position.y;
+	if ( potager.Tomate[x][y].JRepousse <5 ){ //si la tomate est non mure
+		flag=0;
+	}
+	return flag;
+}
+
+int VerifPasPuceron(coord position ,potager potager){
+	int flag=1;
+	int i=0;
+	while(i<potager.nbPuceronVie && flag){
+		puceron PuceTab=potager.EnsPuceron[i];
+		if(position.x==PuceTab.position.x && position.y==PuceTab.position.y){
+			flag=0;
+		}
+		i++;
+	}
+	return flag;
+}
+		
+
--- scr/structure.h
+++ scr/structure.h
@@ -1,9 +1,10 @@
 
 #define N 30 //taille matrice potager
-#define NbPuceron 10 //nombre puceron
+#define NbPuceron 10 //nombre puceron initial
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 
 typedef struct {
@@ -14,26 +15,32 @@ typedef struct {
 		
 typedef struct {
 		 coord position;
-		 int AMange;
-		 int vie;
-		 char mvt;
+		 int AMange;  //nombre de jours consécutifs où le pucerion a mangé
+		 int vie;     // vie du puceron, elle diminue de 1 à chaque tours
+		 int mvt;     // numéro de 0 à 7 correspondant à une direction (voir schéma rendu écrit)
+		 char Dessinmvt; //le caractère à dessiner
 		 
 		 } puceron;
 		 
 typedef struct {
-		 char maturite;
-		 int JRepousse;
+		 char maturite; //le caractère à dessiner
+		 int JRepousse; //le nombre de jour depuis la dernières fois que la tomate a été mangé
 		 
 		 }tomate;
 		 
 typedef struct {
 
-		tomate Tomate[N][N];           
-		puceron EnsPuceron[NbPuceron]; //ensemble puceron
-		int nbPuceronVie;
+		tomate Tomate[N][N];   //le potager avec les informations des tomates        
+		puceron EnsPuceron[N]; //ensemble des pucerons dans le potager
+		int nbPuceronVie;      //nombre de puceron en vie
 		
 		}potager;
 		 
+typedef struct {
 		
+		coord position; //coordonnée de la case
+		int mvt;        // mouvement associé pour l'atteindre ( de 0 à 7 )
+		
+		} caseMvt;
 
 			
